102715a76db48b884b5de5a294845d5d8a5daf04
Md Yamin <mdyamin007@yahoo.com>
2021-07-19 19:02:15 +0600

    bootstrap added

--- js/index.js
+++ js/index.js
@@ -1,12 +1,4 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
-
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbar'
+  })
+  
