c2107e5ad47e44fddd60e5ba1e13d0e28b20ba12
Md Yamin <mdyamin007@yahoo.com>
2021-07-19 20:43:56 +0600

    resume added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+window.addEventListener('load', (e) => {
+    loader = document.getElementById("loading");
+    loader.style.display = "none";
+})
+
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
     target: '#navbar'
   })
